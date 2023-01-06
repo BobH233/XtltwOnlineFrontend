eb10c70729aab3e631a4388e4b3ad7632727e417
BobH <1551608379@qq.com>
2023-01-07 00:56:17 +0800

    修改细节

--- src/components/Table/src/Table.vue
+++ src/components/Table/src/Table.vue
@@ -21,16 +21,6 @@
     <div class="flex items-center table-toolbar-right">
       <!--顶部右侧区域-->
       <slot name="toolbar"></slot>
-
-      <!--斑马纹-->
-      <n-tooltip trigger="hover">
-        <template #trigger>
-          <div class="mr-2 table-toolbar-right-icon">
-            <n-switch v-model:value="isStriped" @update:value="setStriped" />
-          </div>
-        </template>
-        <span>表格斑马纹</span>
-      </n-tooltip>
       <n-divider vertical />
 
       <!--刷新-->
@@ -44,28 +34,6 @@
         </template>
         <span>刷新</span>
       </n-tooltip>
-
-      <!--密度-->
-      <n-tooltip trigger="hover">
-        <template #trigger>
-          <div class="table-toolbar-right-icon">
-            <n-dropdown
-              @select="densitySelect"
-              trigger="click"
-              :options="densityOptions"
-              v-model:value="tableSize"
-            >
-              <n-icon size="18">
-                <ColumnHeightOutlined />
-              </n-icon>
-            </n-dropdown>
-          </div>
-        </template>
-        <span>密度</span>
-      </n-tooltip>
-
-      <!--表格设置单独抽离成组件-->
-      <ColumnSetting />
     </div>
   </div>
   <div class="s-table">
