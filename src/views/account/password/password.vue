0ba456a7956f265ae571bcbdc52d5149a8f14f58
BobH <1551608379@qq.com>
2023-01-07 16:18:48 +0800

    修复致命bug,必须对每个组件外层包上div,否则路由报错

--- src/views/account/password/password.vue
+++ src/views/account/password/password.vue
@@ -1,41 +1,43 @@
 <template>
-  <n-card :bordered="false" title="修改密码">
-    为了保证账户安全，请务必修改默认密码，并注意密码强度。
-  </n-card>
-  <n-card :bordered="false" class="mt-4 proCard">
-    <n-grid cols="1 s:1 m:3 l:3 xl:3 2xl:3" responsive="screen">
-      <n-grid-item offset="0 s:0 m:1 l:1 xl:1 2xl:1">
-        <n-form
-          :label-width="80"
-          :model="formValue"
-          :rules="rules"
-          label-placement="left"
-          ref="formRef"
-          class="py-8"
-        >
-          <n-form-item label="原密码" path="oldPassword">
-            <n-input
-              v-model:value="formValue.oldPassword"
-              placeholder="输入原始密码"
-              type="password"
-              showPasswordOn="click"
-            />
-          </n-form-item>
-          <n-form-item label="新密码" path="newPassword">
-            <n-input
-              v-model:value="formValue.newPassword"
-              placeholder="输入新密码"
-              type="password"
-              showPasswordOn="click"
-            />
-          </n-form-item>
-          <n-space justify="center">
-            <n-button type="primary" @click="formSubmit">确认修改</n-button>
-          </n-space>
-        </n-form>
-      </n-grid-item>
-    </n-grid>
-  </n-card>
+  <div>
+    <n-card :bordered="false" title="修改密码">
+      为了保证账户安全，请务必修改默认密码，并注意密码强度。
+    </n-card>
+    <n-card :bordered="false" class="mt-4 proCard">
+      <n-grid cols="1 s:1 m:3 l:3 xl:3 2xl:3" responsive="screen">
+        <n-grid-item offset="0 s:0 m:1 l:1 xl:1 2xl:1">
+          <n-form
+            :label-width="80"
+            :model="formValue"
+            :rules="rules"
+            label-placement="left"
+            ref="formRef"
+            class="py-8"
+          >
+            <n-form-item label="原密码" path="oldPassword">
+              <n-input
+                v-model:value="formValue.oldPassword"
+                placeholder="输入原始密码"
+                type="password"
+                showPasswordOn="click"
+              />
+            </n-form-item>
+            <n-form-item label="新密码" path="newPassword">
+              <n-input
+                v-model:value="formValue.newPassword"
+                placeholder="输入新密码"
+                type="password"
+                showPasswordOn="click"
+              />
+            </n-form-item>
+            <n-space justify="center">
+              <n-button type="primary" @click="formSubmit">确认修改</n-button>
+            </n-space>
+          </n-form>
+        </n-grid-item>
+      </n-grid>
+    </n-card>
+  </div>
 </template>
 
 <script lang="ts" setup>
